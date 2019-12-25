634dddbab7416280e498ee2d2e69746e0eb7f3a0
Daniel Dawidowski <dvds1987@gmail.com>
2019-12-25 16:55:43 +0100

    Building Search Controler

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,40 @@
-import Search from './models/Search'
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from'./views/base';
+
+// Global State
+// Search object
+// current recipe object
+// shopping list object
+// liked recipes
+
+const state = {};
+
+const controlSearch = async () => {
+    // 1) get query from view
+    const query = searchView.getInput();
+
+    if(query) {
+        // 2) new search object and add to state
+        state.search = new Search(query)
+
+        // 3) Prepare UI for results 
+        searchView.clearInput();
+        searchView.clearResults();
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) render results on UI
+        searchView.renderResults(state.search.result)
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
 
 const search = new Search('pizza');
 console.log(search);
-search.getResults()
+search.getResults();
